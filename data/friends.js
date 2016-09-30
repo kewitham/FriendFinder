7067429acd45c18210bcd1c1d2f4353d520a8ad0
Katie Witham <kewitham@gmail.com>
2016-09-29 20:23:03 -0700

    debugging

--- data/friends.js
+++ data/friends.js
@@ -1,7 +1,7 @@
 var friendArray = [
 	{
-		
 
+		
 	}
 ];
 
--- routing/api-routes.js
+++ routing/api-routes.js
@@ -25,42 +25,45 @@ module.exports = function (app) {
 	// ---------------------------------------------------------------------------
 
 	app.post('/api/friends', function (req, res) {
-		 friendData.push(req.body);
-		 for (i=0; i<(friendData.length -1); i++){
-		 	var newFriend = friendData[friendData.length].questions;
-		 	var friendDiff = [];
-		 	var totalDiff = [];
-		 	for (b=0; b<friendData.questions.length; b++){
-		 	friendDiff.push(Math.abs(newFriend[b] - friendData[i].questions[b]));
-		 	}
-		 	totalDiff.push(friendDiff.reduce(add, 0))
-		 	// Array.min = function(array){
-		 	// 	return Math.min.apply(Math, array);
-		 	// }
-		 	Array.min = function indexOfMin(arr) {
-    			if (arr.length === 0) {
-        		return -1;
-   									 }
+		friendData.push(req.body);
+		 // for (i=0; i<(friendData.length-1); i++){
+		 // 	var newFriend = friendData[friendData.length];
+		 // 	var friendDiff = [];
+		 // 	var totalDiff = [];
+		 // 	for (b=0; b<friendData.questions.length; b++){
+		 // 	friendDiff.push(Math.abs(newFriend[b] - friendData[i].questions[b]));
+		 // 	}
+		 // 	totalDiff.push(friendDiff.reduce(add, 0))
+		 // 	// Array.min = function(array){
+		 // 	// 	return Math.min.apply(Math, array);
+		 // 	// }
+		 // 	Array.min = function indexOfMin(arr) {
+   //  			if (arr.length === 0) {
+   //      		return -1;
+   // 									 }
 
-   			var min = arr[0];
-   			var minIndex = 0;
+   // 			var min = arr[0];
+   // 			var minIndex = 0;
 
-    		for (var i = 1; i < arr.length; i++) {
-        		if (arr[i] < min) {
-            	minIndex = i;
-            	min = arr[i];
-        		}
-    		}
+   //  		for (var i = 1; i < arr.length; i++) {
+   //      		if (arr[i] < min) {
+   //          	minIndex = i;
+   //          	min = arr[i];
+   //      		}
+   //  		}
 
-    		return minIndex;
-			}
+   //  		return minIndex;
+			// }
 		 	
-		 	var bestFriendIndex = Array.min(totalDiff);
-		 	var bestFriend = friendData[bestFriendIndex].survey_name + friendData[bestFriendIndex].survey_photo;
+		 // 	var bestFriendIndex = Array.min(totalDiff);
+		 // 	var bestFriend = friendData[bestFriendIndex].survey_name + friendData[bestFriendIndex].survey_photo;
 
-		 	alert("Your closest match is " + bestFriend)
-		 	//res.json(true); // KEY LINE
-		 }
+		 // 	alert("Your closest match is " + bestFriend)
+		 	
+
+		 	//res.json(false); // KEY LINE
+		 //}
+		 //friendData.push(req.body);
 	});
 
 	// ---------------------------------------------------------------------------
